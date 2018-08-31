bf3d8b36dd8b316dbf48a59997250e6b4ac59f39
Peter Xenopoulos <pxenopoulos@10-18-20-41.dynapool.wireless.nyu.edu>
2018-08-31 13:58:29 -0400

    added player data frame func

--- gen_data.py
+++ gen_data.py
@@ -5,6 +5,7 @@
 
 import numpy as np
 import pandas as pd
+import re
 
 from requests import get
 from requests.exceptions import RequestException
@@ -17,7 +18,7 @@ def get_page(url):
 	'''
 	try:
 		with closing(get(url, stream=True)) as resp:
-			if is_good_response(resp):
+			if good_response(resp):
 				return resp.content
 			else:
 				return None
@@ -35,9 +36,45 @@ def good_response(resp):
 		and content_type.find('html') > -1)
 
 def log_error(e):
-	'''
-	Function to simply print the error
+	''' Function to simply print the error
 	@param String e: Error string
 	'''
 	print(e)
+
+def get_script_nodes():
+	''' Function to return the text of the script for each year's page
+	Keep years and script_position constant -- these are hardcoded by necessity
+	'''
+	years = [2012, 2013, 2014, 2015, 2016, 2017]
+	script_position = [16, 14, 14, 14, 14, 14]
+	script_text = []
+	for i in range(6):
+		y = years[i]
+		pos = script_position[i]
+		url = "http://www.nfl.com/draft/{0}/tracker#dt-tabs:dt-by-round/dt-by-grade-input:14/dt-by-round-input:1".format(y)
+		raw_html = get_page(url)
+		html = BeautifulSoup(raw_html, 'html.parser')
+		script_nodes = html.select('script')
+		script_text.append(str(script_nodes[pos]))
+	return script_text
+
+def write_player_df(script_text):
+	''' Function to write a data frame containing player information
+	@param List script_text: A list containing the pertinent javascript
+	'''
+	player_data_frames = []
+	for s in script_text:
+		player_text = re.findall(r'{"personId"(.*?)}', s)
+		playerId, firstName, lastName, hasAnalysis, pos, expertGrade, pick = [], [], [], [], [], [], []
+		for p in player_text:
+			playerId.append(re.findall(r':(.*?),' ,p)[0])
+			firstName.append(re.findall(r'"firstName":"(.*?)",' ,p)[0])
+			lastName.append(re.findall(r'"lastName":"(.*?)",' ,p)[0])
+			hasAnalysis.append(re.findall(r'"hasAnalysis":(.*?),' ,p)[0])
+			pos.append(re.findall(r'"pos":"(.*?)",' ,p)[0])
+			expertGrade.append(re.findall(r'"expertGrade":(.*?),' ,p)[0])
+			pick.append(re.findall(r'"pick":(.*?),' ,p)[0])
+		player_dict = {"player_id": playerId, "first_name": firstName, "last_name": lastName, "hasAnalysis": hasAnalysis, "position": pos ,"grade": expertGrade, "pick": pick}
+		player_data_frames.append(pd.DataFrame.from_dict(player_dict))
+	return player_data_frames
 
